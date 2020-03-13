e4c20652bd8732a114e3d4cbe41f173162555210
ooanishoo <anish.maharjan12341@gmail.com>
2020-03-13 20:50:00 +1100

    Import all the required data for the calcualtor from options file

---
+++ lib/data/options.dart
@@ -0,0 +1,22 @@
+import 'package:smart_select/smart_select.dart';
+
+List<SmartSelectOption<String>> visaSubclass = [
+  SmartSelectOption<String>(value: '188', title: 'Subclass 188'),
+  SmartSelectOption<String>(value: '189', title: 'Subclass 189'),
+  SmartSelectOption<String>(value: '190', title: 'Subclass 190'),
+  SmartSelectOption<String>(value: '489', title: 'Subclass 489'),
+  SmartSelectOption<String>(value: '491', title: 'Subclass 491'),
+];
+
+List<SmartSelectOption<String>> age = [
+  SmartSelectOption<String>(value: '188', title: '18-25 years'),
+  SmartSelectOption<String>(value: '189', title: '18-33 years'),
+  SmartSelectOption<String>(value: '190', title: '18-40 years'),
+  SmartSelectOption<String>(value: '489', title: '40-45 years'),
+];
+
+List<SmartSelectOption<String>> language = [
+  SmartSelectOption<String>(value: '10', title: 'Competent English'),
+  SmartSelectOption<String>(value: '10', title: 'Proficient English'),
+  SmartSelectOption<String>(value: '20', title: 'Superior English'),
+];
