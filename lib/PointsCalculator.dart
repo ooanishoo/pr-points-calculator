bea57adfedcefdee0acee7087cb55aef1f3db082
ooanishoo <anish.maharjan12341@gmail.com>
2020-03-14 19:35:13 +1100

    Add calculatePoints method to update the points, Add updatePoints callback function to pass totalpoints to parent class Homepage whenever user updates the form fields

--- lib/PointsCalculator.dart
+++ lib/PointsCalculator.dart
@@ -2,24 +2,51 @@ import 'package:flutter/material.dart';
 import 'package:smart_select/smart_select.dart';
 import './data/options.dart' as options;
 
-
 class PointsCalculator extends StatefulWidget {
+  const PointsCalculator({Key key, this.updatePoints}) : super(key: key);
+
   @override
   _PointsCalculatorState createState() => _PointsCalculatorState();
+
+  final void Function(int) updatePoints;
 }
 
 class _PointsCalculatorState extends State<PointsCalculator> {
   String subclass;
-  String age;
-  String language;
-  String overseasSkillEmployment;
-  String australianSkillEmployment;
-  String educationalQualification;
-  String studyInAustralia;
-  String specialistEducationQualification;
-  String ccl;
-  String studyRegional;
-  String partnerSkills;
+  String age = '0';
+  String language = '0';
+  String overseasSkillEmployment = '0';
+  String australianSkillEmployment = '0';
+  String educationalQualification = '0';
+  String studyInAustralia = '0';
+  String specialistEducationQualification = '0';
+  String ccl = '0';
+  String studyRegional = '0';
+  String partnerSkills = '0';
+
+  int points = 0;
+
+  _PointsCalculatorState();
+
+  void calculatePoints() {
+    print('calculating points');
+    print('before calculating' + this.points.toString());
+    print(age);
+    setState(() {
+      points = int.parse(this.age) +
+          int.parse(this.language) +
+          int.parse(this.overseasSkillEmployment) +
+          int.parse(this.australianSkillEmployment) +
+          int.parse(this.educationalQualification) +
+          int.parse(this.studyInAustralia) +
+          int.parse(this.specialistEducationQualification) +
+          int.parse(this.ccl) +
+          int.parse(this.studyRegional) +
+          int.parse(this.partnerSkills);
+    });
+    print('after calculating' + this.points.toString());
+    widget.updatePoints(points);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -29,25 +56,37 @@ class _PointsCalculatorState extends State<PointsCalculator> {
           title: 'Visa subclass',
           value: subclass,
           options: options.visaSubclass,
-          isTwoLine: true,
-          onChange: (val) => setState(() => subclass = val),
+          isTwoLine: false,
+          onChange: (val) {
+            setState(() => subclass = val);
+            calculatePoints();
+          },
           modalType: SmartSelectModalType.bottomSheet,
+          trailing: Text(''),
         ),
         SmartSelect<String>.single(
           title: 'Age',
           value: age,
           options: options.age,
           isTwoLine: true,
-          onChange: (val) => setState(() => age = val),
+          onChange: (val) {
+            setState(() => age = val);
+            calculatePoints();
+          },
           modalType: SmartSelectModalType.bottomSheet,
+          trailing: Text(age),
         ),
         SmartSelect<String>.single(
           title: 'English Language',
           value: language,
           options: options.language,
           isTwoLine: true,
-          onChange: (val) => setState(() => language = val),
+          onChange: (val) {
+            setState(() => language = val);
+            calculatePoints();
+          },
           modalType: SmartSelectModalType.bottomSheet,
+          trailing: Text(language),
         ),
         ListTile(
             title: Text(
@@ -59,24 +98,36 @@ class _PointsCalculatorState extends State<PointsCalculator> {
           value: overseasSkillEmployment,
           options: options.overseasSkillEmployment,
           isTwoLine: true,
-          onChange: (val) => setState(() => overseasSkillEmployment = val),
+          onChange: (val) {
+            setState(() => overseasSkillEmployment = val);
+            calculatePoints();
+          },
           modalType: SmartSelectModalType.bottomSheet,
+          trailing: Text(overseasSkillEmployment),
         ),
         SmartSelect<String>.single(
           title: 'Australian skilled employment',
           value: australianSkillEmployment,
           options: options.australianSkillEmployment,
           isTwoLine: true,
-          onChange: (val) => setState(() => australianSkillEmployment = val),
+          onChange: (val) {
+            setState(() => australianSkillEmployment = val);
+            calculatePoints();
+          },
           modalType: SmartSelectModalType.bottomSheet,
+          trailing: Text(australianSkillEmployment),
         ),
         SmartSelect<String>.single(
           title: 'Educational qualifications',
           value: educationalQualification,
           options: options.educationalQualification,
           isTwoLine: true,
-          onChange: (val) => setState(() => educationalQualification = val),
+          onChange: (val) {
+            setState(() => educationalQualification = val);
+            calculatePoints();
+          },
           modalType: SmartSelectModalType.bottomSheet,
+          trailing: Text(educationalQualification),
         ),
         ListTile(
             title: Text(
@@ -88,41 +139,60 @@ class _PointsCalculatorState extends State<PointsCalculator> {
           value: studyInAustralia,
           options: options.studyInAustralia,
           isTwoLine: true,
-          onChange: (val) => setState(() => studyInAustralia = val),
+          onChange: (val) {
+            setState(() => studyInAustralia = val);
+            calculatePoints();
+          },
           modalType: SmartSelectModalType.bottomSheet,
+          trailing: Text(studyInAustralia),
         ),
         SmartSelect<String>.single(
           title: 'Specialist education qualification ?',
           value: specialistEducationQualification,
           options: options.specialistEducationQualification,
           isTwoLine: true,
-          onChange: (val) =>
-              setState(() => specialistEducationQualification = val),
+          onChange: (val) {
+            setState(() => specialistEducationQualification = val);
+            calculatePoints();
+          },
           modalType: SmartSelectModalType.bottomSheet,
+          trailing: Text(specialistEducationQualification),
         ),
         SmartSelect<String>.single(
           title: 'Have you done NAATI ?',
           value: ccl,
           options: options.ccl,
           isTwoLine: true,
-          onChange: (val) => setState(() => ccl = val),
+          onChange: (val) {
+            setState(() => ccl = val);
+            calculatePoints();
+          },
           modalType: SmartSelectModalType.bottomSheet,
+          trailing: Text(ccl),
         ),
         SmartSelect<String>.single(
           title: 'Study in regional Australia ?',
           value: studyRegional,
           options: options.studyRegional,
           isTwoLine: true,
-          onChange: (val) => setState(() => studyRegional = val),
+          onChange: (val) {
+            setState(() => studyRegional = val);
+            calculatePoints();
+          },
           modalType: SmartSelectModalType.bottomSheet,
+          trailing: Text(studyRegional),
         ),
         SmartSelect<String>.single(
           title: 'Partner Skill ?',
           value: partnerSkills,
           options: options.partnerSkills,
           isTwoLine: true,
-          onChange: (val) => setState(() => partnerSkills = val),
+          onChange: (val) {
+            setState(() => partnerSkills = val);
+            calculatePoints();
+          },
           modalType: SmartSelectModalType.bottomSheet,
+          trailing: Text(partnerSkills),
         ),
       ],
     );
