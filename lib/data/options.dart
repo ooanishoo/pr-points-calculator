7b946f59b88f9c7a456d0a83aaa3cacb139d5fa8
ooanishoo <anish.maharjan12341@gmail.com>
2020-03-14 19:28:34 +1100

    Update the file with correct points for each field

--- lib/data/options.dart
+++ lib/data/options.dart
@@ -9,65 +9,87 @@ List<SmartSelectOption<String>> visaSubclass = [
 ];
 
 List<SmartSelectOption<String>> age = [
-  SmartSelectOption<String>(value: '188', title: 'at least 18 but less than 25 years'),
-  SmartSelectOption<String>(value: '189', title: 'at least 25 but less than 33 years'),
-  SmartSelectOption<String>(value: '190', title: 'at least 33 but less than 40 years'),
-  SmartSelectOption<String>(value: '489', title: 'at least 40 but less than 45 years'),
+  SmartSelectOption<String>(
+      value: '25', title: 'at least 18 but less than 25 years'),
+  SmartSelectOption<String>(
+      value: '30', title: 'at least 25 but less than 33 years'),
+  SmartSelectOption<String>(
+      value: '25', title: 'at least 33 but less than 40 years'),
+  SmartSelectOption<String>(
+      value: '15', title: 'at least 40 but less than 45 years'),
 ];
 
 List<SmartSelectOption<String>> language = [
-  SmartSelectOption<String>(value: '10', title: 'Competent English'),
+  SmartSelectOption<String>(value: '20', title: 'Competent English'),
   SmartSelectOption<String>(value: '10', title: 'Proficient English'),
-  SmartSelectOption<String>(value: '20', title: 'Superior English'),
+  SmartSelectOption<String>(value: '0', title: 'Superior English'),
 ];
 
 List<SmartSelectOption<String>> overseasSkillEmployment = [
-  SmartSelectOption<String>(value: '10', title:'Less than 3 years'),
-  SmartSelectOption<String>(value: '10', title:'At least 3 but less than 5 years'),
-  SmartSelectOption<String>(value: '20', title:'At least 5 but less than 8 years'),
-  SmartSelectOption<String>(value: '20', title:'At least 8 years')
+  SmartSelectOption<String>(value: '0', title: 'Less than 3 years'),
+  SmartSelectOption<String>(
+      value: '5', title: 'At least 3 but less than 5 years'),
+  SmartSelectOption<String>(
+      value: '10', title: 'At least 5 but less than 8 years'),
+  SmartSelectOption<String>(value: '15', title: 'At least 8 years')
 ];
 
 List<SmartSelectOption<String>> australianSkillEmployment = [
-  SmartSelectOption<String>(value: '10', title:'Less than 1 year'),
-  SmartSelectOption<String>(value: '10', title:'At least 1 but less than 3 years'),
-  SmartSelectOption<String>(value: '20', title:'At least 3 but less than 5 years'),
-  SmartSelectOption<String>(value: '20', title:'At least 5 but less than 8 years'),
-  SmartSelectOption<String>(value: '20', title:'At least 8 years')
+  SmartSelectOption<String>(value: '10', title: 'Less than 1 year'),
+  SmartSelectOption<String>(
+      value: '5', title: 'At least 1 but less than 3 years'),
+  SmartSelectOption<String>(
+      value: '10', title: 'At least 3 but less than 5 years'),
+  SmartSelectOption<String>(
+      value: '15', title: 'At least 5 but less than 8 years'),
+  SmartSelectOption<String>(value: '20', title: 'At least 8 years')
 ];
 
 List<SmartSelectOption<String>> educationalQualification = [
-  SmartSelectOption<String>(value: '10', title:'A Doctorate from an Australian educational institution or a Doctorate from another educational institution, that is of a recognised standard'),
-  SmartSelectOption<String>(value: '10', title:'At least a Bachelor degree from an Australian educational institution or at least a Bachelor qualification, from another educational institution, that is of a recognised standard'),
-  SmartSelectOption<String>(value: '20', title:'A diploma or trade qualification from an Australian educational institution'),
-  SmartSelectOption<String>(value: '20', title:'Attained a qualification or award recognised by the relevant assessing authority for your nominated skilled occupation as being suitable for that occupation'),
-  SmartSelectOption<String>(value: '20', title:'Not applicable')
+  SmartSelectOption<String>(
+      value: '20',
+      title:
+          'A Doctorate from an Australian educational institution or a Doctorate from another educational institution, that is of a recognised standard'),
+  SmartSelectOption<String>(
+      value: '15',
+      title:
+          'At least a Bachelor degree from an Australian educational institution or at least a Bachelor qualification, from another educational institution, that is of a recognised standard'),
+  SmartSelectOption<String>(
+      value: '10',
+      title:
+          'A diploma or trade qualification from an Australian educational institution'),
+  SmartSelectOption<String>(
+      value: '10',
+      title:
+          'Attained a qualification or award recognised by the relevant assessing authority for your nominated skilled occupation as being suitable for that occupation'),
+  SmartSelectOption<String>(value: '0', title: 'Not applicable')
+];
+
+List<SmartSelectOption<String>> specialistEducationQualification = [
+  SmartSelectOption<String>(value: '10', title: 'Yes'),
+  SmartSelectOption<String>(value: '0', title: 'No'),
 ];
 
 List<SmartSelectOption<String>> studyInAustralia = [
-  SmartSelectOption<String>(value: '5', title:'Yes'),
-  SmartSelectOption<String>(value: '0', title:'No'),
+  SmartSelectOption<String>(value: '5', title: 'Yes'),
+  SmartSelectOption<String>(value: '0', title: 'No'),
 ];
-List<SmartSelectOption<String>> specialistEducationQualification = [
-  SmartSelectOption<String>(value: '5', title:'Yes'),
-  SmartSelectOption<String>(value: '0', title:'No'),
+
+List<SmartSelectOption<String>> professionalYear = [
+  SmartSelectOption<String>(value: '5', title: 'Yes'),
+  SmartSelectOption<String>(value: '0', title: 'No'),
 ];
 
 List<SmartSelectOption<String>> ccl = [
-  SmartSelectOption<String>(value: '5', title:'Yes'),
-  SmartSelectOption<String>(value: '0', title:'No'),
+  SmartSelectOption<String>(value: '5', title: 'Yes'),
+  SmartSelectOption<String>(value: '0', title: 'No'),
 ];
 
 List<SmartSelectOption<String>> studyRegional = [
-  SmartSelectOption<String>(value: '5', title:'Yes'),
-  SmartSelectOption<String>(value: '0', title:'No'),
+  SmartSelectOption<String>(value: '5', title: 'Yes'),
+  SmartSelectOption<String>(value: '0', title: 'No'),
 ];
 List<SmartSelectOption<String>> partnerSkills = [
-  SmartSelectOption<String>(value: '5', title:'Yes'),
-  SmartSelectOption<String>(value: '0', title:'No'),
+  SmartSelectOption<String>(value: '10', title: 'Yes'),
+  SmartSelectOption<String>(value: '0', title: 'No'),
 ];
-
-
-
-
-
